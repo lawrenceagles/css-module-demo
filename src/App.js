eb928298ec7cfc5b981afcba9cd727f7bd5959bb
lawrenceagles <lawrenceagles@gmail.com>
2021-04-11 16:16:02 +0100

    added app.module.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import { title, app } from './App.module.css';
 import Button from './components/Button';
 
 function App() {
 	return (
 		<div>
-			<h1 className="title">CSS Module Buttons</h1>
-			<article className="app">
+			<h1 className={title}>CSS Module Buttons</h1>
+			<article className={app}>
 				<Button />
 			</article>
 		</div>
